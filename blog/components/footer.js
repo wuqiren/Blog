3f6dd65be8fc2c3ca6beda3410e9a0edbb1cdaa8
fishfan <fishfan@tencent.com>
2019-11-03 13:48:00 +0800

    finished Web

---
+++ blog/components/footer.js
@@ -0,0 +1,8 @@
+import '../public/style/components/footer.css'
+const Footer=()=>(
+    <div className="footer-div">
+        <div>系统是由React+Node+Ant Design</div>
+        <div>jsfei.com</div>
+    </div>
+)
+export default Footer;
